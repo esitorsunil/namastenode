74e22d7336b15a2101b979b42a49ec26c65cfc1b
esitorsunil <sunilrio23@gmail.com>
2024-12-21 18:56:38 +0530

    module.export &  require

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+const {x , calculateSum} = require('./xyz')
+
 var name = "Sunil"
 
 var a = 10
@@ -5,3 +7,6 @@ var b = 20
 
 console.log(a + b)
 console.log(name)
+
+calculateSum(10,30)
+console.log(x);
