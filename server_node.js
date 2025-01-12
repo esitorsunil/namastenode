21203e4c4691311d0e89ae6fa9377f25db7332bf
esitorsunil <sunilrio23@gmail.com>
2025-01-12 17:41:56 +0530

    update server

--- server_node.js
+++ server_node.js
@@ -1,11 +1,11 @@
 const http = require("node:http");
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(function (req, res) {
   if(req.url === "/getSecretData") {
     res.end("There is no secret data, hahaha");
   }
   res.end("My First Server");
-})
+});
 
 
 server.listen(3000);
